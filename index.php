d2b825e3a7c254f8e302ddc49dec55fc3ca5de04
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-12 12:20:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
-echo "Hello World!";?>
+  $name = 0;
 
-<form action="" method="GET">
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  }
+  echo $name;
+?>
+<form action="" method="POST">
   <input type="text" name="name" placeholder="ダルビッシュと入力してください">
   <input type="submit" value="submit">
 </form>
