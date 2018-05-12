f2247ba0a4348e56c7669544e653efa645c481ba
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-12 13:02:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $_SESSION['count'] = $_SESSION['count'] + 1;
   }
+  echo $_SESSION['count'];
 ?>
 <form action="" method="POST">
   <input type="text" name="name" placeholder="ダルビッシュと入力してください">
