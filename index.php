a0bbb23efe58437eef5cae372acfcc6d63ebb603
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-12 22:50:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-  session_start();
-  if(!array_key_exists('count', $_SESSION)) {
-    $_SESSION['count'] = 0;
-  }
-  if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $_SESSION['count'] = $_SESSION['count'] + 1;
-  }
-  echo $_SESSION['count'];
+  //session_start();
+  //if(!array_key_exists('count', $_SESSION)) {
+  //  $_SESSION['count'] = 0;
+  //}
+  //if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  //  $_SESSION['count'] = $_SESSION['count'] + 1;
+  //}
+  //echo $_SESSION['count'];
   
   $data = "https://spreadsheets.google.com/feeds/list/18DtL1BZ7KvPToRWVPgJ4EYcH8q2HK9WPs-ruUAShJf4/od6/public/values?alt=json";
   $json = file_get_contents($data);
