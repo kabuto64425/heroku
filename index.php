3c9221fbbbc4703b02cc3060b18eece7afdfbb6c
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-13 00:45:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,6 @@
   //}
 ?>
 <form action="" method="POST">
-  <input type="text" name="name" placeholder="ダルビッシュと入力してください">
   <input type="submit" value="submit">
   <input type="hidden" name="count" value="<?php echo ($count+1);?>">
 </form>
-<?php echo $count; ?>
