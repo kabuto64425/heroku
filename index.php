fb5926ca0186f27532c96d770a3306b920acc555
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-13 18:13:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  //session_start();
+  session_start();
   //if(!array_key_exists('count', $_SESSION)) {
   //  $_SESSION['count'] = 0;
   //}
@@ -14,11 +14,17 @@
     $count = $_POST['count'];
   }
 
-  $data = "https://spreadsheets.google.com/feeds/list/18DtL1BZ7KvPToRWVPgJ4EYcH8q2HK9WPs-ruUAShJf4/od6/public/values?alt=json";
-  $json = file_get_contents($data);
-  $json_decode = json_decode($json);
+  if(!array_key_exists('names', $_SESSION)) {
+    $data = "https://spreadsheets.google.com/feeds/list/18DtL1BZ7KvPToRWVPgJ4EYcH8q2HK9WPs-ruUAShJf4/od6/public/values?alt=json";
+    $json = file_get_contents($data);
+    $json_decode = json_decode($json);
+    
+    $names = $json_decode->feed->entry;
+    $_SESSION['names'] = $names;
+  } else {
+    $names = $_SESSION['names'];
+  }
   
-  $names = $json_decode->feed->entry;
   echo $names[$count]->{'gsx$問題文'}->{'$t'};
   //foreach ($names as $name) {
   //  echo $name->{'gsx$問題文'}->{'$t'};
