450c553c56d73a4fe250e713ca977223dfc9bed0
kabuto64425 <higasumi52@yahoo.co.jp>
2018-05-13 18:43:43 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
   session_start();
-  //if(!array_key_exists('count', $_SESSION)) {
-  //  $_SESSION['count'] = 0;
-  //}
-  //if($_SERVER['REQUEST_METHOD'] === 'POST'){
-  //  $_SESSION['count'] = $_SESSION['count'] + 1;
-  //}
-  //echo $_SESSION['count'];
   
-  if(!array_key_exists('count', $_POST)) {
-    $count = 0;
-  } else {
-    $count = $_POST['count'];
-  }
-
   if(!array_key_exists('names', $_SESSION)) {
     $data = "https://spreadsheets.google.com/feeds/list/18DtL1BZ7KvPToRWVPgJ4EYcH8q2HK9WPs-ruUAShJf4/od6/public/values?alt=json";
     $json = file_get_contents($data);
@@ -25,13 +12,18 @@
     $names = $_SESSION['names'];
   }
   
+  if(!array_key_exists('count', $_SESSION)) {
+    $_SESSION['count'] = 0;
+  }
+
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $_SESSION['count'] = $_SESSION['count'] + 1;
+  }
+
+  $count = $_SESSION['count'];
+  
   echo $names[$count]->{'gsx$問題文'}->{'$t'};
-  //foreach ($names as $name) {
-  //  echo $name->{'gsx$問題文'}->{'$t'};
-  //  echo '</br>';
-  //}
 ?>
 <form action="" method="POST">
   <input type="submit" value="submit">
-  <input type="hidden" name="count" value="<?php echo ($count+1);?>">
 </form>
